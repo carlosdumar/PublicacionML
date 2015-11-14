283c404b0e9f54cc4057831f80d7123bbc01469b
carlos <adminDreamBike@github.com>
2015-11-14 11:43:39 -0500

    Se agrega el cuerpo de la publicacion

--- js/app.js
+++ js/app.js
@@ -28,8 +28,30 @@
 		$scope.entry = 'Ingresar';
 		$scope.registry = 'Registrarse';		
 	}]);
-	
-	app.controller('specificationsController', ['$scope', function() {
 
+	app.controller('SpecificationsController', ['$scope', function ($scope) {
+		$scope.titleSpec = 'LAMPARA DE 5 LEDS PARA BICICLETA'
+		$scope.mainImage = 'image/mainImage.jpg'
+		$scope.specifications = [
+			{title: 'A prueba de agua'},
+			{title: 'Con tecnologia de centrado'},
+			{title: 'Soporte desmontable'},
+			{title: 'Modo de operación: continuo o Flash, modo de cambio pulsando el botón en la parte superior'},
+			{title: 'Multi-funcional: la luz de la bici, Antorcha, buceo (hasta 20 metros / 66 ft.)'}, 
+			{title: 'De larga duración: hasta 150 horas de funcionamiento con 4 pilas "AAA" (no incluidas)'},
+			{title: 'Color: Negro'},
+			{title: 'Peso: 46.6g'}
+		];
+		$scope.images = [
+			{url: 'image/secondImage1.jpg'},
+			{url: 'image/secondImage2.jpg'},
+			{url: 'image/secondImage3.jpg'},
+			{url: 'image/secondImage4.jpg'}
+		];
+	}]);
+	app.controller('PackageIncludeController', ['$scope', function ($scope) {
+		$scope.details = [
+			{title: '1 x Bike Head Light'}
+		];
 	}]);
 })();
