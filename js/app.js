c92f81f3879789a873a4df148e559a203924c01b
carlos <adminDreamBike@github.com>
2015-11-16 17:10:10 -0500

    Se modifica el archivo app.js para agregar la publicacion de solarstorm

--- js/app.js
+++ js/app.js
@@ -30,28 +30,37 @@
 	}]);
 
 	app.controller('SpecificationsController', ['$scope', function ($scope) {
-		$scope.titleSpec = 'LAMPARA DE 5 LEDS PARA BICICLETA'
-		$scope.mainImage = 'image/mainImage.jpg'
+		$scope.titleSpec = 'Solarstorm 5000 Lumen Led Frontal Bicicleta Luz De Bicicleta para Carretera'
+		$scope.mainImage = 'image/l2.jpg'
 		$scope.specifications = [
-			{title: 'A prueba de agua'},
-			{title: 'Con tecnologia de centrado'},
-			{title: 'Soporte desmontable'},
-			{title: 'Modo de operación: continuo o Flash, modo de cambio pulsando el botón en la parte superior'},
-			{title: 'Multi-funcional: la luz de la bici, Antorcha, buceo (hasta 20 metros / 66 ft.)'}, 
-			{title: 'De larga duración: hasta 150 horas de funcionamiento con 4 pilas "AAA" (no incluidas)'},
-			{title: 'Color: Negro'},
-			{title: 'Peso: 46.6g'}
+			{title: 'Vida LED: 50.000 horas'},
+			{title: 'Luz mas condensada, larga exposición.'},
+			{title: '4 modos de interrumpción: Debil, Medio, Fuerte, Strober.'},
+			{title: 'Tipo de Interruptor: botón azul.'},
+			{title: 'Duración: 2-3 horas.'}, 
+			{title: 'Localización del interruptor: Cola-casquillo.'},
+			{title: 'De alto rendimiento, interruptor electrónico.'},
+			{title: 'Haga click en el interruptor para encender / apagar luz'},
+			{title: 'Haga click en el interruptor para cambiar los modo de brillo, BAJO-MEDIO-ALTO'},
+			{title: 'Mantenga pulsado el interruptor durante más de un segundo para acceder al modo Strobe.'},
+			{title: 'Indicador de la bateria (junto al botón azul)'},
+			{title: 'Tres luces encendidas en power > 80%'},
+			{title: 'Dos luces encendidas en 50 < power < 80%'},
+			{title: 'Fuente de alimentación: 1x8.4v 6400mAh'}
 		];
 		$scope.images = [
-			{url: 'image/secondImage1.jpg'},
-			{url: 'image/secondImage2.jpg'},
-			{url: 'image/secondImage3.jpg'},
-			{url: 'image/secondImage4.jpg'}
+			{url: 'image/l10.jpg'},
+			{url: 'image/l9.jpg'},
+			{url: 'image/l11.jpg'},
+			{url: 'image/l6.jpg'}
 		];
 	}]);
 	app.controller('PackageIncludeController', ['$scope', function ($scope) {
 		$scope.details = [
-			{title: '1 x Bike Head Light'}
+			{title: '1 x 5000LM faro unidad del CREE T6 LED'},
+			{title: '2 x Anillo de goma para la instalación'},			
+			{title: '1 x Cargador de CA 100-240v'},
+			{title: '1 x Headband'}
 		];
 	}]);
 })();
